378a1c55c7545690d49aff8bfafe7a63e16523de
SAIFUL ISLAM (Fahim) <saifulislamfahim.me@gmail.com>
2025-05-26 00:45:00 +0600

    dynamic memory

---
+++ dynamic_variable.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x;
+    x = 10;
+    int *p = new int;
+    cout << p << endl;
+    *p = 100;
+    cout << *p << endl;
+
+    return 0;
+}
