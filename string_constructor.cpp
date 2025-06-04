4dd8b56a8c5566f7061b731692fa4932902c2d7d
SAIFUL ISLAM (Fahim) <saifulislamfahim.me@gmail.com>
2025-06-05 01:02:17 +0600

    string constructor range

---
+++ string_constructor.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //string s = "Hello";
+
+    //calling step one
+    //string s("Hello World");
+
+    //way to two
+    //string s("Hello World", 5); // this way string will be resize
+
+    //way three
+    string s = "Welcome To";
+    string t(s, 5);
+
+    cout << t << endl;
+
+    return 0;
+}
