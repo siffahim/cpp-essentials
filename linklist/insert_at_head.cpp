0e1b9fea2c04d36ed388d3b40b1418e09ef7b297
SAIFUL ISLAM (Fahim) <saifulislamfahim.me@gmail.com>
2025-07-12 00:52:37 +0600

    insert at head

---
+++ linklist/insert_at_head.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* next;
+    
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_head(Node* &head, int val){
+    Node *newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+}
+
+void print_linklist(Node* head) {
+    Node *temp = head;
+    while(temp != NULL){
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+}
+
+int main(){
+    Node *head = new Node(10);
+    Node *a = new Node(50);
+    Node *b = new Node(75);
+
+    head->next = a;
+    a->next = b;
+
+    insert_at_head(head, 100);
+
+    print_linklist(head);
+
+    return 0;
+}
