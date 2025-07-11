d2d40a7cd2debee8479b2b26ea8943735902f4ff
siffahim <siffahim25gmail.com>
2025-07-11 20:21:21 +0600

    update: 

---
+++ linklist/print_linklist.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int val;
+        Node *next;
+    
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+int main(){
+    Node *head = new Node(10);
+    Node *b = new Node(20);
+    Node *c = new Node(30);
+    Node *d = new Node(40);
+
+    
+
+    return 0;
+}
